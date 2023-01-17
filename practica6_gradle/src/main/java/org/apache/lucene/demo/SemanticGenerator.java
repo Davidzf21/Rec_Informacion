92549dac10646fba5a552626cc042cea9df89c69
AcquaDiGiorgio <774248@unizar.es>
2023-01-17 12:41:43 +0100

    quasi finalizado semantic generator

--- practica6_gradle/src/main/java/org/apache/lucene/demo/SemanticGenerator.java
+++ practica6_gradle/src/main/java/org/apache/lucene/demo/SemanticGenerator.java
@@ -1,5 +1,8 @@
 package org.apache.lucene.demo;
+import com.github.jsonldjava.utils.Obj;
+import jdk.jshell.spi.ExecutionControl;
 import org.apache.commons.io.FileUtils;
+import org.apache.jena.base.Sys;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.ReadWrite;
 import org.apache.jena.rdf.model.*;
@@ -7,17 +10,16 @@ import org.apache.jena.rdf.model.*;
 import org.apache.jena.tdb2.TDB2Factory;
 import org.apache.jena.util.FileManager;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import openllet.jena.PelletReasonerFactory;
 import org.apache.jena.vocabulary.RDF;
 import org.w3c.dom.Node;
 
+import javax.swing.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -73,6 +75,8 @@ public class SemanticGenerator {
     Model union = ModelFactory.createUnion(modelo, skos);
 
     HashMap<String, Property> mapaPropiedades = new HashMap<>();
+    mapaPropiedades.put("identifier", modelo.getProperty("zaguanVoc:identificador"));
+    mapaPropiedades.put("relation", modelo.getProperty("zaguanVoc:relacion"));
     mapaPropiedades.put("title", modelo.getProperty("zaguanVoc:titulo"));
     mapaPropiedades.put("creator", modelo.getProperty("zaguanVoc:creador"));
     mapaPropiedades.put("subject", modelo.getProperty("zaguanVoc:tema"));
@@ -81,27 +85,47 @@ public class SemanticGenerator {
     mapaPropiedades.put("contributor", modelo.getProperty("zaguanVoc:contribuidor"));
     mapaPropiedades.put("date", modelo.getProperty("zaguanVoc:fecha"));
 
+    mapaPropiedades.put("nombreEditor", modelo.getProperty("zaguanVoc:nombreEditor"));
+    mapaPropiedades.put("nombrePerona", modelo.getProperty("zaguanVoc:nombrePerona"));
+    mapaPropiedades.put("apellido", modelo.getProperty("zaguanVoc:apellido"));
+
+    mapaPropiedades.put("Persona", modelo.getProperty("zaguanVoc:Persona"));
+
+
+    ArrayList<RDFNode> tesauro = new ArrayList<>();
+    NodeIterator it = skos.listObjects();
+    while(it.hasNext())
+    {
+      RDFNode no = it.next();
+      if(!no.toString().startsWith("http"))
+      {
+        tesauro.add(no);
+      }
+    }
+
     // Creamos un modelo de inferencia OWL2
     // InfModel inf = ModelFactory.createInfModel(PelletReasonerFactory.theInstance().create(), modelo);
 
     // Añadimos las instancias al modelo
-    Model modeloFinal = cargarDocumentos(union, mapaPropiedades, new File(docsPath));
-    modeloFinal.write(new FileOutputStream(new File("librosColeccionInf.ttl")),"TURTLE");
+    Model coleccion = ModelFactory.createDefaultModel();
+    Model modeloFinal = cargarDocumentos(coleccion, tesauro, mapaPropiedades, new File(docsPath));
+    union = ModelFactory.createUnion(union, modeloFinal);
+
+
+    union.write(new FileOutputStream(new File("librosColeccionInf.ttl")),"TURTLE");
   }
 
-  static Model cargarDocumentos(Model modelo, HashMap<String, Property> propiedades, File file) throws Exception {
+  static Model cargarDocumentos(Model modelo, ArrayList<RDFNode> tesauro, HashMap<String, Property> propiedades, File file) throws Exception {
     if (file.canRead()) {
       if (file.isDirectory()) {
         String[] files = file.list();
         if (files != null) {
           for (int i = 0; i < files.length; i++) {
-            cargarDocumentos(modelo, propiedades, new File(file, files[i]));
+            cargarDocumentos(modelo, tesauro, propiedades, new File(file, files[i]));
           }
         }
       } else {
         Resource documento = modelo.createResource("http://www.practicaZaguan.com/zaguan/" + file.getName());
-        // Ruta del fichero file.getPath();
-        // Ultima modificación file.lastModified();
 
         // Creamos el parser
         DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
@@ -109,11 +133,9 @@ public class SemanticGenerator {
 
         String[] tipos = new String[]{
                 "dc:title",
-                "dc:creator",
-                "dc:subject",
-                //"dc:description",
-                "dc:publisher",
-                "dc:contributor"
+                "dc:identifier",
+                "dc:relation",
+                //"dc:description"
         };
 
         // Tipos
@@ -124,6 +146,65 @@ public class SemanticGenerator {
             String content = nodo.getTextContent();
             documento.addProperty(propiedades.get(tipo.substring(3)), content);
           }
+        }
+
+        String[] personas = new String[]{
+                "dc:creator",
+                "dc:contributor"
+        };
+
+        for(String persona : personas) {
+          org.w3c.dom.NodeList nodosPersona = doc2.getElementsByTagName(persona);
+          for (int i = 0; i < nodosPersona.getLength(); i++) {
+            Node nodo = nodosPersona.item(i);
+            String content = nodo.getTextContent();
+            String[] fullName = content.split(", ");
+
+            if (fullName.length == 2) {
+              documento.addProperty(propiedades.get(persona.substring(3)),
+                      modelo.createResource()
+                              .addProperty(propiedades.get("nombrePerona"), fullName[1])
+                              .addProperty(propiedades.get("apellido"), fullName[0])
+              );
+            }else{
+              documento.addProperty(propiedades.get(persona.substring(3)),
+                      modelo.createResource()
+                              .addProperty(propiedades.get("nombrePerona"), fullName[0])
+              );
+            }
+          }
+        }
+
+        org.w3c.dom.NodeList nodosPublisher = doc2.getElementsByTagName("dc:publisher");
+        for (int i =0; i<nodosPublisher.getLength(); i++){
+          Node nodo = nodosPublisher.item(i);
+          String content = nodo.getTextContent();
+
+          String[] publishers = content.split("; ");
+          for(String publisher : publishers)
+            documento.addProperty(propiedades.get("publisher"),
+                    modelo.createResource()
+                            .addProperty(propiedades.get("nombreEditor"), publisher)
+            );
+        }
+
+        org.w3c.dom.NodeList listaNodos = doc2.getElementsByTagName("dc:subject");
+        for (int i =0; i<listaNodos.getLength(); i++){
+          Node nodo = listaNodos.item(i);
+          String content = nodo.getTextContent();
+
+          RDFNode match = null;
+          for(RDFNode tipo : tesauro)
+          {
+            if(tipo.toString().equals(content))
+            {
+              match = tipo;
+              break;
+            }
+          }
+
+          if(match != null)
+            documento.addProperty(propiedades.get("subject"), match);
         }
 
         // Fecha
@@ -156,7 +237,7 @@ public class SemanticGenerator {
             tipo = "tesis";
           }
 
-          documento.addProperty(RDF.type, tipo);
+          documento.addProperty(RDF.type, "zaguanVoc:<" + tipo + ">");
         }
       }
     }
